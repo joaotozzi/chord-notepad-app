1c2e2b1401c2b789240afe3fcc555aa8ad13daec
João Tozzi <joaotozzi@gmail.com>
2022-12-04 16:31:52 -0300

    ajuste url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://chord-notepad-api.herokuapp.com/cifras/"
+    baseURL: "http://ec2-3-83-69-203.compute-1.amazonaws.com:8080/cifras/"
 });
 
 export default api;
